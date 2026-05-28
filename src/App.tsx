545488f30f6cb2f72b1012eb2ee71d0aaebac388
wokeuplikethis* <94878181+Zodiac411@users.noreply.github.com>
2026-05-28 23:36:02 +0100

    Fix GitHub Pages route basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AppShell } from './components/layout/AppShell'
 
+const routerBasename = import.meta.env.BASE_URL.replace(/\/$/, '') || '/'
+
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={routerBasename}>
       <Routes>
         <Route path="/*" element={<AppShell />} />
       </Routes>
